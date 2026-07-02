023f14ea611e0173dca8b1642526be9ef2ca3a09
Wem <20968054+Wemino@users.noreply.github.com>
2026-07-02 08:39:08 +0200

    FixAspectRatio: Show the black bars on the death screen

--- src/MainLoop/BlackBarGuard.cpp
+++ src/MainLoop/BlackBarGuard.cpp
@@ -3,35 +3,37 @@
 
 namespace BlackBarGuard
 {
-	void Tick()
-	{
-		static int menuState = -1;
+    void Tick()
+    {
+        static int menuState = -1;
 
-		AAlicePlayerController* pc = g_State.AlicePlayerController;
-		if (!pc) return;
+        AAlicePlayerController* pc = g_State.AlicePlayerController;
+        if (!pc) return;
 
-		UAlicePlayerInput* input = (UAlicePlayerInput*)pc->PlayerInput;
-		bool inputDisabled = input && input->bDisableInputInCinematic;
-		bool active = !pc->bInMainMenu && !pc->bCinematicMode && !inputDisabled;
+        APawn* pawn = pc->Pawn;
+        UAlicePlayerInput* input = (UAlicePlayerInput*)pc->PlayerInput;
+        bool inputDisabled = input && input->bDisableInputInCinematic;
+        bool dead = pawn && pawn->Health <= 0;
+        bool active = !pc->bInMainMenu && !pc->bCinematicMode && !inputDisabled && !dead;
 
-		bool canMove = active && !pc->IsPaused();
+        bool canMove = active && !pc->IsPaused();
 
-		int now = canMove ? 1 : 0;
-		if (menuState == -1)
-		{
-			menuState = now;
-		}
-		else if (now != menuState)
-		{
-			menuState = now;
-			g_State.shouldBlockBlackBar = (now == 1);
-		}
+        int now = canMove ? 1 : 0;
+        if (menuState == -1)
+        {
+            menuState = now;
+        }
+        else if (now != menuState)
+        {
+            menuState = now;
+            g_State.shouldBlockBlackBar = (now == 1);
+        }
 
-		g_State.bRealGameplay = active && pc->Pawn;
+        g_State.bRealGameplay = active && pawn;
 
-		if (g_State.isWideScreen)
-		{
-			g_State.AliceEngine->ConstrainedAspectRatio = g_State.bRealGameplay ? g_State.currentAspectRatio : ASPECT_RATIO_16_9;
-		}
-	}
+        if (g_State.isWideScreen)
+        {
+            g_State.AliceEngine->ConstrainedAspectRatio = g_State.bRealGameplay ? g_State.currentAspectRatio : ASPECT_RATIO_16_9;
+        }
+    }
 }
